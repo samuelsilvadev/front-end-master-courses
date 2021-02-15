2847d874f010688b485e72e6027a718e19e3c810
Samuel Silva <samuelsilvawb@gmail.com>
2021-02-15 21:14:05 +0000

    refactor(intro-react-v5): Update Pet component to render more information about a pet

--- intro-react-v5/src/components/pet/Pet.js
+++ intro-react-v5/src/components/pet/Pet.js
@@ -1,20 +1,38 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Pet = ({ name, type, breed }) => {
+const DEFAULT_IMG_PLACEHOLDER = "http://placecorgi.com/300/300";
+
+const Pet = ({ id, name, breed, medias, location, type }) => {
   return (
-    <section>
-      <h1>{name}</h1>
-      <h2>{type}</h2>
-      <h2>{breed}</h2>
-    </section>
+    <a href={`/details/${id}`} className="pet">
+      <section>
+        <picture className="image-container">
+          <img src={medias?.[0]?.small ?? DEFAULT_IMG_PLACEHOLDER} alt={name} />
+        </picture>
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>{`${type} - ${breed} - ${location}`}</h2>
+        </div>
+      </section>
+    </a>
   );
 };
 
 Pet.propTypes = {
-  name: PropTypes.string,
-  type: PropTypes.string,
-  breed: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  breed: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  type: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  medias: PropTypes.arrayOf(
+    PropTypes.shape({
+      small: PropTypes.string,
+      medium: PropTypes.string,
+      large: PropTypes.string,
+      full: PropTypes.string,
+    })
+  ),
 };
 
 export default Pet;
