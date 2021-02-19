eb0101eb8e80b482b5f69663955bdb8e044f84f2
Samuel Silva <samuelsilvawb@gmail.com>
2021-02-19 07:38:42 +0000

    feat(into-react-v5): Add Details component implementation

--- intro-react-v5/src/components/details/Details.js
+++ intro-react-v5/src/components/details/Details.js
@@ -1,11 +1,77 @@
 import React from "react";
+import PropTypes from "prop-types";
+import petAPI from "@frontendmasters/pet";
 
-const Details = () => {
-  return (
-    <div className="details">
-      <h1>Hello, I am a detail page</h1>
-    </div>
-  );
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      hasApiError: false,
+    };
+  }
+
+  async componentDidMount() {
+    const { id } = this.props;
+
+    if (id) {
+      const parsedId = +id;
+
+      try {
+        const { animal } = await petAPI.animal(parsedId);
+        const { name, photos, type, description, contact, breeds } = animal;
+
+        this.setState({
+          isLoading: false,
+          hasApiError: false,
+          name,
+          medias: photos,
+          type,
+          description,
+          location: `${contact.address.city}, ${contact.address.state}`,
+          breed: breeds?.primary,
+        });
+      } catch {
+        this.setState({ isLoading: false, hasApiError: true });
+      }
+    }
+  }
+
+  render() {
+    if (this.state.hasApiError) {
+      return (
+        <section className="details">
+          <h1>Something went wrong</h1>
+        </section>
+      );
+    }
+
+    if (this.state.isLoading) {
+      return (
+        <section className="details">
+          <h1>Loading</h1>
+        </section>
+      );
+    }
+
+    const { type, name, breed, location, description } = this.state;
+
+    return (
+      <section className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${type} - ${breed} - ${location}`}</h2>
+          <button>Adopt me</button>
+          <p>{description}</p>
+        </div>
+      </section>
+    );
+  }
+}
+
+Details.propTypes = {
+  id: PropTypes.string,
 };
 
 export default Details;
