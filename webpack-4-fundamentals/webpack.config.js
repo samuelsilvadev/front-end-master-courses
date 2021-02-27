05f1c716408de416da96119cf6008d9668c3bc2f
Samuel Silva <samuelsilvawb@gmail.com>
2021-02-27 22:52:35 +0000

    feat(webpack-4-fundamentals): Compose configs based on environment
    - Added different configs file to each environment.
    - Cleaned up dist folder before generate a production build.

--- webpack-4-fundamentals/webpack.config.js
+++ webpack-4-fundamentals/webpack.config.js
@@ -1,10 +1,20 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpackMerge = require('webpack-merge');
+
+const webpackConfig = (mode) => require(`./config/webpack.${mode}`)(mode);
+
 module.exports = (env) => {
 	const { mode = 'none' } = env;
 
-	return {
-		mode,
-		output: {
-			filename: 'bundle.js',
+	return webpackMerge.merge(
+		{
+			mode,
+			output: {
+				filename: 'bundle.js',
+			},
+			plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
 		},
-	};
+		webpackConfig(mode)
+	);
 };
