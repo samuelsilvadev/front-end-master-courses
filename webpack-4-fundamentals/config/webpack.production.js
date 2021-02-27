f69b87cc7ded362b19685bdfefe0349660466058
Samuel Silva <samuelsilvawb@gmail.com>
2021-02-27 22:59:09 +0000

    refactor(webpack-4-fundamentals): Remove rimraf and use `CleanWebpackPlugin`

--- webpack-4-fundamentals/config/webpack.production.js
+++ webpack-4-fundamentals/config/webpack.production.js
@@ -1,5 +1,8 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = () => ({
 	output: {
 		filename: '[name].[chunkhash].js',
 	},
+	plugins: [new CleanWebpackPlugin()],
 });
