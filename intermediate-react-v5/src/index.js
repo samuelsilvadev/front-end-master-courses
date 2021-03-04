5cb7471d9f9d438c71cbeab77aa16fcb340855a2
Samuel Silva <samuelsilvawb@gmail.com>
2021-03-04 21:13:47 +0000

    feat(intermediate-react-v5): Move Header to a component and style it with emotion

--- intermediate-react-v5/src/index.js
+++ intermediate-react-v5/src/index.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
-import { Link, Router } from "@reach/router";
+import { Router } from "@reach/router";
 
 import SearchParams from "components/search-params";
 import Details from "components/details";
 
 import { ThemeContext } from "hooks/theme";
+import Header from "components/header";
 
 const App = () => {
   const theme = useState({ buttonColor: "green" });
@@ -14,9 +15,7 @@ const App = () => {
     <React.StrictMode>
       <ThemeContext.Provider value={theme}>
         <div>
-          <header>
-            <Link to="/">Adopt me</Link>
-          </header>
+          <Header />
           <Router>
             <Details path="/details/:id" />
             <SearchParams path="/" />
